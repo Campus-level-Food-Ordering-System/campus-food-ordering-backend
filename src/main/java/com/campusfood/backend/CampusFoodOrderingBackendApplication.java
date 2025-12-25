95a039cc6f097e82e45352c4a9c8c7104ae203af
Akhilesh2309 <akileshkrishna6688@gmail.com>
2025-12-25 12:27:16 +0530

    Inital backend setup

---
+++ src/main/java/com/campusfood/backend/CampusFoodOrderingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.campusfood.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusFoodOrderingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusFoodOrderingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campusfood/backend/CampusFoodOrderingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campusfood.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusFoodOrderingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
